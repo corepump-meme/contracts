844035252f50267c1fbe6b7e636681caf471ae29
joseph emmanuel kayode (iemarjay) <emarjay921@gmail.com>
2025-07-22 12:06:02 +0100

    Added coreTestnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+console.log("Using private key:", PRIVATE_KEY ? "****" : "No private key set");
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -15,6 +21,10 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    coreTestnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: [PRIVATE_KEY],
     },
     // Core Chain configuration can be added here later
   },
